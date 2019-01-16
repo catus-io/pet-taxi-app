4823e545a1f205e504a445c43994d11e58a2183d
ADAMS <nerdrun.adams@gmail.com>
2019-01-16 11:00:46 +0900

    #1 making signup-style design

--- app/components/Auth.vue
+++ app/components/Auth.vue
@@ -1,28 +1,30 @@
 <template>
-  <Page>
+  <Page actionBarHidden="true">
     <FlexboxLayout class="auth-w">
       <StackLayout class="auth-c">
-        <Image src="~/images/pet-logo.png" stretch="none" class="logo"></Image>
-        <Label text="PET TAXI"></Label>
-        <GridLayout rows="auto, auto, auto, auto">
-          <StackLayout row="0" class="input-field">
-            <TextField class="input" v-model="email" hint="email come on" keyboardType="email" returnKeyType="next"/>
+        <GridLayout class="auth-p" rows="auto, auto, auto, auto, auto">
+          <StackLayout row="0">
+            <Image src="~/images/pet-logo.png" class="logo"></Image>
+            <Label text="PET TEXI" class="app-name-p"></Label>
           </StackLayout>
           <StackLayout row="1" class="input-field">
-            <TextField class="input" v-model="nickname" hint="nickname come on" returnKeyType="next"/>
+            <TextField class="input" v-model="email" hint="email come on" keyboardType="email" returnKeyType="next"/>
           </StackLayout>
           <StackLayout row="2" class="input-field">
-            <TextField class="input" v-model="password" hint="password come on" secure="true" returnKeyType="next"/>
+            <TextField class="input" v-model="nickname" hint="nickname come on" returnKeyType="next"/>
           </StackLayout>
           <StackLayout row="3" class="input-field">
+            <TextField class="input" v-model="password" hint="password come on" secure="true" returnKeyType="next"/>
+          </StackLayout>
+          <StackLayout row="4" class="input-field">
             <TextField class="input" v-model="repassword" hint="repassword come on" secure="true" returnKeyType="done"/>
+          </StackLayout>
+          <StackLayout row="5">
+            <Label text="너는 이미 회원이다"/>
+            <Label text="Login"/>
           </StackLayout>
         </GridLayout>
         <Button text="Button" @tap="onSubmit" />
-      </StackLayout>
-      <StackLayout>
-        <Label text="너는 이미 회원이다"/>
-        <Label text="Login"/>
       </StackLayout>
     </FlexboxLayout>
   </Page>
@@ -47,14 +49,24 @@ export default {
 <style lang="scss" scoped>
 .auth-w {
   flex-direction: column;
+  align-items: center;
   .auth-c {
-    .logo {
-      height: 90;
-    }
-    .input-field {
-      margin-bottom: 25;
-      .input {
-        placeholder-color: #A8A8A8;
+    margin-left: 30;
+    margin-right: 30;
+    vertical-align: middle;
+    .auth-p {
+      .logo {
+        height: 90;
+      }
+      .app-name-p {
+        font-size: 25;
+        font-weight: bold;
+      }
+      .input-field {
+        margin-bottom: 25;
+        .input {
+          // placeholder-color: #A8A8A8;
+        }
       }
     }
   }
