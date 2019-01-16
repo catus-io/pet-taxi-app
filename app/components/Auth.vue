cc29bf4b5584837fb605592e42d1c015f636c9ae
ADAMS <nerdrun.adams@gmail.com>
2019-01-16 14:43:48 +0900

    #1 modify signup layout

--- app/components/Auth.vue
+++ app/components/Auth.vue
@@ -2,31 +2,35 @@
   <Page actionBarHidden="true">
     <FlexboxLayout class="auth-w">
       <StackLayout class="auth-c">
-        <GridLayout class="auth-p" rows="auto, auto, auto, auto, auto">
-          <StackLayout row="0">
-            <Image src="~/images/pet-logo.png" class="logo"></Image>
-            <Label text="PET TEXI" class="app-name-p"></Label>
+        <GridLayout class="auth-p" rows="150,auto,*">
+          <StackLayout row="0" class="auth-title-i" orientation="horizontal">
+            <!-- <Image src="~/images/pet-logo.png" class="logo"></Image> -->
+            <Label width="100%" text="PET TAXI" class="app-name-p"></Label>
           </StackLayout>
-          <StackLayout row="1" class="input-field">
-            <TextField class="input" v-model="email" hint="email come on" keyboardType="email" returnKeyType="next"/>
+          <StackLayout row="1" class="auth-input-i">
+            <StackLayout class="input-field">
+              <TextField class="input" v-model="email" hint="email come on" keyboardType="email" returnKeyType="next"/>
+            </StackLayout>
+            <StackLayout class="input-field" v-show="isSignup">
+              <TextField class="input" v-model="nickname" hint="nickname come on" returnKeyType="next"/>
+            </StackLayout>
+            <StackLayout class="input-field">
+              <TextField class="input" v-model="password" hint="password come on" secure="true" returnKeyType="next"/>
+            </StackLayout>
+            <StackLayout class="input-field" v-show="isSignup">
+              <TextField class="input" v-model="repassword" hint="repassword come on" secure="true" returnKeyType="done"/>
+            </StackLayout>
           </StackLayout>
-          <StackLayout row="2" class="input-field" v-show="isSignup">
-            <TextField class="input" v-model="nickname" hint="nickname come on" returnKeyType="next"/>
-          </StackLayout>
-          <StackLayout row="3" class="input-field">
-            <TextField class="input" v-model="password" hint="password come on" secure="true" returnKeyType="next"/>
-          </StackLayout>
-          <StackLayout row="4" class="input-field" v-show="isSignup">
-            <TextField class="input" v-model="repassword" hint="repassword come on" secure="true" returnKeyType="done"/>
+          <StackLayout row="2">
+            <Button :text="isSignup ? '회원가입' : '로그인'" @tap="onSubmit" />
+            <Label class="login-p" @tap="toggleAuth">
+              <FormattedString>
+                <span :text="isSignup ? '이미 회원이시라면' : '비밀번호를 잊으셨다면' "/>
+                <span :text="isSignup ? '로그인' : '회원가입'" />
+              </FormattedString>
+            </Label>
           </StackLayout>
         </GridLayout>
-        <Button :text="isSignup ? '회원가입' : '로그인'" @tap="onSubmit" />
-        <Label class="login-p" @tap="toggleAuth">
-          <FormattedString>
-            <span :text="isSignup ? '이미 회원이시라면' : '비밀번호를 잊으셨다면' "/>
-            <span :text="isSignup ? '로그인' : '회원가입'" />
-          </FormattedString>
-        </Label>
       </StackLayout>
     </FlexboxLayout>
   </Page>
@@ -69,6 +73,8 @@ export default {
 .auth-w {
   flex-direction: column;
   align-items: center;
+  background: url("~/images/pet-taxi-auth-bg.jpg");
+  background-size: cover;
   .auth-c {
     margin-left: 30;
     margin-right: 30;
@@ -86,11 +92,15 @@ export default {
         font-size: 25;
         font-weight: bold;
         text-align: center;
+        justify-self: center;
       }
       .input-field {
         margin-bottom: 20;
         .input {
-          // placeholder-color: #A8A8A8;
+          width: 60%;
+          padding: 5;
+          border-bottom: 1px soild #333;
+          background-color: #f1f1f1;
         }
       }
     }
